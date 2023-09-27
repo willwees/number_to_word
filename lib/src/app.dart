f278e89f5af5355bf727a3761bf67373c3ca0552
williams <william.suyanto@koltiva.com>
2023-09-27 20:28:08 +0700

    refactor: separate App from main

---
+++ lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:number_to_word/src/core/core.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Converter App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
