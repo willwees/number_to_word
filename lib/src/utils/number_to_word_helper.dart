20c5129a6724d4b7a1c84059dd5c4eea207c30e3
williams <william.suyanto@koltiva.com>
2023-09-28 11:14:04 +0700

    feat: add NumberToWordHelper

---
+++ lib/src/utils/number_to_word_helper.dart
@@ -0,0 +1,77 @@
+class NumberToWordHelper {
+  static final List<String> _numberWordList = <String>[
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  static final List<String> _tensWordList = <String>[
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  static final List<String> _thousandsWordList = <String>[
+    '',
+    'thousand',
+    'million',
+    'billion',
+    'trillion',
+  ];
+
+  static String convertNumberToWord(int number) {
+    if (number == 0) {
+      return 'zero';
+    }
+
+    String result = '';
+    int testedNumber = number;
+    int thousandsIndex = 0;
+
+    while (testedNumber > 0) {
+      final int remainder = testedNumber % 1000;
+
+      if (remainder < 20) {
+        result = '${_numberWordList[remainder]} ${_thousandsWordList[thousandsIndex]} $result';
+      } else if (remainder < 100) {
+        result = '${_tensWordList[remainder ~/ 10]}-${_numberWordList[remainder % 10]} ${_thousandsWordList[thousandsIndex]} $result';
+      } else {
+        if (remainder % 100 == 0) {
+          result = '${_numberWordList[remainder ~/ 100]} hundred ${_thousandsWordList[thousandsIndex]} $result';
+        } else {
+          result =
+              '${_numberWordList[remainder ~/ 100]} hundred ${_tensWordList[remainder % 100 ~/ 10]}-${_numberWordList[remainder % 10]} ${_thousandsWordList[thousandsIndex]} $result';
+        }
+      }
+
+      testedNumber = testedNumber ~/ 1000;
+      thousandsIndex++;
+    }
+
+    return result.trim();
+  }
+}
